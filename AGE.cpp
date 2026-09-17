b5bd661ae780b7b96584239cdc63cde19855184b
ParthT1306 <parththaware814@gmail.com>
2026-09-17 16:38:19 +0530

    Add files via upload

---
+++ AGE.cpp
@@ -0,0 +1,27 @@
+#include<iostream> 
+using namespace std;
+int main(){
+
+//variable define
+int age = 25;
+cout<<"Parths age is :"<<age<<endl;
+
+//declaration
+//int age ;
+//cout<<age;
+
+//definetion
+//int age = 21;
+//cout<<age<<endl;
+
+//manipulation or updation
+age = 102;
+cout<<age<<endl;
+
+
+
+
+
+
+return 0;
+}
